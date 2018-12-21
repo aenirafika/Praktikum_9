5cef494542b79ba775e39cc40a72ad938297af15
aenirafika <36910850+aenirafika@users.noreply.github.com>
2018-12-21 17:08:20 +0700

    Add files via upload

---
+++ Truk.java
@@ -0,0 +1,21 @@
+package Praktikum9;
+
+class Truk {
+
+    Truk(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getMuatanMaks() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void tambahMuatan(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getMuatan() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
